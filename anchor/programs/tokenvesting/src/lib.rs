f854cfc7d20a1b9f8da7577ef20de7ddeecdb461
JKrishnaD <jayak5063@gmail.com>
2025-04-16 18:10:01 +0530

    contract done

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -1,70 +1,234 @@
-#![allow(clippy::result_large_err)]
-
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{ Mint, TokenAccount, TokenInterface, TransferChecked },
+};
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod tokenvesting {
+    use anchor_spl::token_interface;
+
     use super::*;
 
-  pub fn close(_ctx: Context<CloseTokenvesting>) -> Result<()> {
-    Ok(())
-  }
+    pub fn create_vesting_account(
+        ctx: Context<CreateVestingAccount>,
+        company_name: String
+    ) -> Result<()> {
+        *ctx.accounts.vesting_account = VestingAccount {
+            owner: ctx.accounts.signer.key(),
+            mint: ctx.accounts.mint.key(),
+            treasure_token_account: ctx.accounts.treasure_token_account.key(),
+            company_name,
+            treasury_bump: ctx.bumps.treasure_token_account,
+            bump: ctx.bumps.vesting_account,
+        };
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.tokenvesting.count = ctx.accounts.tokenvesting.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+        Ok(())
+    }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.tokenvesting.count = ctx.accounts.tokenvesting.count.checked_add(1).unwrap();
-    Ok(())
-  }
+    pub fn create_employee_account(
+        ctx: Context<CreateEmployeeAccount>,
+        start_time: i64,
+        end_time: i64,
+        total_amount: u64,
+        cliff_time: i64
+    ) -> Result<()> {
+        *ctx.accounts.employee_account = EmployeeAccount {
+            benificiary: ctx.accounts.benificiary.key(),
+            start_time,
+            end_time,
+            cliff_time,
+            total_amount,
+            total_withdraw: ctx.accounts.employee_account.total_withdraw,
+            vesting_account: ctx.accounts.vesting_account.key(),
+            bump: ctx.bumps.employee_account,
+        };
+        Ok(())
+    }
 
-  pub fn initialize(_ctx: Context<InitializeTokenvesting>) -> Result<()> {
-    Ok(())
-  }
+    pub fn claim_tokens(ctx: Context<ClaimTokens>, _company_name: String) -> Result<()> {
+        let employee_account = &mut ctx.accounts.employee_account;
+        let now = Clock::get()?.unix_timestamp;
 
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.tokenvesting.count = value.clone();
-    Ok(())
-  }
-}
+        if now < employee_account.cliff_time {
+            return Err(Errors::ClaimNotAvailable.into());
+        }
 
-#[derive(Accounts)]
-pub struct InitializeTokenvesting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+        let time_since_start = now.saturating_sub(employee_account.start_time);
+        let total_vesting_time = employee_account.end_time.saturating_sub(
+            employee_account.start_time
+        );
 
-  #[account(
-  init,
-  space = 8 + Tokenvesting::INIT_SPACE,
-  payer = payer
-  )]
-  pub tokenvesting: Account<'info, Tokenvesting>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseTokenvesting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+        if total_vesting_time == 0 {
+            return Err(Errors::InvalidVestingTime.into());
+        }
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub tokenvesting: Account<'info, Tokenvesting>,
-}
+        let vested_amount = if now >= employee_account.end_time {
+            employee_account.total_amount
+        } else {
+            match employee_account.total_amount.checked_mul(time_since_start as u64) {
+                Some(product) => product / (total_vesting_time as u64),
+                None => {
+                    return Err(Errors::CalculationOverflow.into());
+                }
+            }
+        };
+        let claimable_amount = vested_amount.saturating_sub(employee_account.total_withdraw);
+        if claimable_amount == 0 {
+            return Err(Errors::NothingToClaim.into());
+        }
 
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub tokenvesting: Account<'info, Tokenvesting>,
+        let transfer_cpi_accounts = TransferChecked {
+            from: ctx.accounts.treasure_token_account.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.employe_token_account.to_account_info(),
+            authority: ctx.accounts.treasure_token_account.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+
+        let signer_seeds: &[&[&[u8]]] = &[
+            &[
+                b"vesting_treasure",
+                ctx.accounts.vesting_account.company_name.as_ref(),
+                &[ctx.accounts.vesting_account.treasury_bump],
+            ],
+        ];
+
+        let cpi_context = CpiContext::new(cpi_program, transfer_cpi_accounts).with_signer(
+            signer_seeds
+        );
+        let decimals = ctx.accounts.mint.decimals;
+        token_interface::transfer_checked(cpi_context, claimable_amount, decimals)?;
+
+        employee_account.total_withdraw += claimable_amount;
+        Ok(())
+    }
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Tokenvesting {
-  count: u8,
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasure_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub bump: u8,
+}
+
+#[account]
+pub struct EmployeeAccount {
+    pub benificiary: Pubkey,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub cliff_time: i64,
+    pub vesting_account: Pubkey,
+    pub total_amount: u64,
+    pub total_withdraw: u64,
+    pub bump: u8,
+}
+
+#[derive(Accounts)]
+#[instruction(company_name :String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        space = 8 + VestingAccount::INIT_SPACE,
+        payer = signer,
+        seeds = [company_name.as_ref()],
+        bump
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        token::mint = mint,
+        token::authority = treasure_token_account,
+        payer = signer,
+        seeds = [b"vesting_treasure", company_name.as_bytes()],
+        bump
+    )]
+    pub treasure_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub benificiary: SystemAccount<'info>,
+
+    #[account(has_one = owner)]
+    pub vesting_account: Account<'info, VestingAccount>,
+    #[account(
+        init,
+        space = 8 + 104,
+        payer = owner,
+        seeds = [b"employee_vesting", benificiary.key().as_ref(), vesting_account.key().as_ref()],
+        bump
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(company_name:String)]
+pub struct ClaimTokens<'info> {
+    #[account(mut)]
+    pub benificiary: Signer<'info>,
+
+    #[account(mut,
+        seeds = [b"employee_vesting",benificiary.key().as_ref(),vesting_account.key().as_ref()],
+        bump = employee_account.bump,
+        has_one = benificiary,
+        has_one = vesting_account,
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+
+    #[account(mut, 
+        seeds = [company_name.as_ref()],
+        bump = vesting_account.bump,
+        has_one = treasure_token_account,
+        has_one = mint 
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(mut)]
+    pub treasure_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = benificiary,
+        associated_token::mint = mint,
+        associated_token::authority = benificiary,
+        associated_token::token_program = token_program
+    )]
+    pub employe_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[error_code]
+pub enum Errors {
+    #[msg("claim is not available yet")]
+    ClaimNotAvailable,
+    #[msg("claim is not available yet")]
+    InvalidVestingTime,
+    #[msg("claculation overflow")]
+    CalculationOverflow,
+    #[msg("nothing to claim")]
+    NothingToClaim,
 }
--- anchor/target/types/tokenvesting.ts
+++ anchor/target/types/tokenvesting.ts
@@ -5,108 +5,468 @@
  * IDL can be found at `target/idl/tokenvesting.json`.
  */
 export type Tokenvesting = {
-  address: 'coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF'
-  metadata: {
-    name: 'tokenvesting'
-    version: '0.1.0'
-    spec: '0.1.0'
-    description: 'Created with Anchor'
-  }
-  instructions: [
+  "address": "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF",
+  "metadata": {
+    "name": "tokenvesting",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: 'close'
-      discriminator: [98, 165, 201, 177, 108, 65, 206, 96]
-      accounts: [
+      "name": "claimTokens",
+      "discriminator": [
+        108,
+        216,
+        210,
+        231,
+        0,
+        212,
+        42,
+        64
+      ],
+      "accounts": [
         {
-          name: 'payer'
-          writable: true
-          signer: true
+          "name": "benificiary",
+          "writable": true,
+          "signer": true,
+          "relations": [
+            "employeeAccount"
+          ]
         },
         {
-          name: 'tokenvesting'
-          writable: true
-        },
-      ]
-      args: []
-    },
-    {
-      name: 'decrement'
-      discriminator: [106, 227, 168, 59, 248, 27, 150, 101]
-      accounts: [
-        {
-          name: 'tokenvesting'
-          writable: true
-        },
-      ]
-      args: []
-    },
-    {
-      name: 'increment'
-      discriminator: [11, 18, 104, 9, 104, 174, 59, 33]
-      accounts: [
-        {
-          name: 'tokenvesting'
-          writable: true
-        },
-      ]
-      args: []
-    },
-    {
-      name: 'initialize'
-      discriminator: [175, 175, 109, 31, 13, 152, 155, 237]
-      accounts: [
-        {
-          name: 'payer'
-          writable: true
-          signer: true
-        },
-        {
-          name: 'tokenvesting'
-          writable: true
-          signer: true
+          "name": "employeeAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  101,
+                  109,
+                  112,
+                  108,
+                  111,
+                  121,
+                  101,
+                  101,
+                  95,
+                  118,
+                  101,
+                  115,
+                  116,
+                  105,
+                  110,
+                  103
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "benificiary"
+              },
+              {
+                "kind": "account",
+                "path": "vestingAccount"
+              }
+            ]
+          }
         },
         {
-          name: 'systemProgram'
-          address: '11111111111111111111111111111111'
-        },
-      ]
-      args: []
-    },
-    {
-      name: 'set'
-      discriminator: [198, 51, 53, 241, 116, 29, 126, 194]
-      accounts: [
-        {
-          name: 'tokenvesting'
-          writable: true
-        },
-      ]
-      args: [
-        {
-          name: 'value'
-          type: 'u8'
-        },
-      ]
-    },
-  ]
-  accounts: [
-    {
-      name: 'tokenvesting'
-      discriminator: [255, 176, 4, 245, 188, 253, 124, 25]
-    },
-  ]
-  types: [
-    {
-      name: 'tokenvesting'
-      type: {
-        kind: 'struct'
-        fields: [
-          {
-            name: 'count'
-            type: 'u8'
+          "name": "vestingAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "arg",
+                "path": "companyName"
+              }
+            ]
           },
+          "relations": [
+            "employeeAccount"
+          ]
+        },
+        {
+          "name": "mint",
+          "relations": [
+            "vestingAccount"
+          ]
+        },
+        {
+          "name": "treasureTokenAccount",
+          "writable": true,
+          "relations": [
+            "vestingAccount"
+          ]
+        },
+        {
+          "name": "employeTokenAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "benificiary"
+              },
+              {
+                "kind": "account",
+                "path": "tokenProgram"
+              },
+              {
+                "kind": "account",
+                "path": "mint"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "tokenProgram"
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        },
+        {
+          "name": "associatedTokenProgram",
+          "address": "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+        }
+      ],
+      "args": [
+        {
+          "name": "companyName",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "createEmployeeAccount",
+      "discriminator": [
+        94,
+        118,
+        255,
+        19,
+        171,
+        159,
+        58,
+        107
+      ],
+      "accounts": [
+        {
+          "name": "owner",
+          "writable": true,
+          "signer": true,
+          "relations": [
+            "vestingAccount"
+          ]
+        },
+        {
+          "name": "benificiary"
+        },
+        {
+          "name": "vestingAccount"
+        },
+        {
+          "name": "employeeAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  101,
+                  109,
+                  112,
+                  108,
+                  111,
+                  121,
+                  101,
+                  101,
+                  95,
+                  118,
+                  101,
+                  115,
+                  116,
+                  105,
+                  110,
+                  103
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "benificiary"
+              },
+              {
+                "kind": "account",
+                "path": "vestingAccount"
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "startTime",
+          "type": "i64"
+        },
+        {
+          "name": "endTime",
+          "type": "i64"
+        },
+        {
+          "name": "totalAmount",
+          "type": "u64"
+        },
+        {
+          "name": "cliffTime",
+          "type": "i64"
+        }
+      ]
+    },
+    {
+      "name": "createVestingAccount",
+      "discriminator": [
+        129,
+        178,
+        2,
+        13,
+        217,
+        172,
+        230,
+        218
+      ],
+      "accounts": [
+        {
+          "name": "signer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "vestingAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "arg",
+                "path": "companyName"
+              }
+            ]
+          }
+        },
+        {
+          "name": "mint"
+        },
+        {
+          "name": "treasureTokenAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  118,
+                  101,
+                  115,
+                  116,
+                  105,
+                  110,
+                  103,
+                  95,
+                  116,
+                  114,
+                  101,
+                  97,
+                  115,
+                  117,
+                  114,
+                  101
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "companyName"
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        },
+        {
+          "name": "tokenProgram"
+        }
+      ],
+      "args": [
+        {
+          "name": "companyName",
+          "type": "string"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "employeeAccount",
+      "discriminator": [
+        65,
+        245,
+        87,
+        188,
+        58,
+        86,
+        209,
+        151
+      ]
+    },
+    {
+      "name": "vestingAccount",
+      "discriminator": [
+        102,
+        73,
+        10,
+        233,
+        200,
+        188,
+        228,
+        216
+      ]
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "claimNotAvailable",
+      "msg": "claim is not available yet"
+    },
+    {
+      "code": 6001,
+      "name": "invalidVestingTime",
+      "msg": "claim is not available yet"
+    },
+    {
+      "code": 6002,
+      "name": "calculationOverflow",
+      "msg": "claculation overflow"
+    },
+    {
+      "code": 6003,
+      "name": "nothingToClaim",
+      "msg": "nothing to claim"
+    }
+  ],
+  "types": [
+    {
+      "name": "employeeAccount",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "benificiary",
+            "type": "pubkey"
+          },
+          {
+            "name": "startTime",
+            "type": "i64"
+          },
+          {
+            "name": "endTime",
+            "type": "i64"
+          },
+          {
+            "name": "cliffTime",
+            "type": "i64"
+          },
+          {
+            "name": "vestingAccount",
+            "type": "pubkey"
+          },
+          {
+            "name": "totalAmount",
+            "type": "u64"
+          },
+          {
+            "name": "totalWithdraw",
+            "type": "u64"
+          },
+          {
+            "name": "bump",
+            "type": "u8"
+          }
         ]
       }
     },
+    {
+      "name": "vestingAccount",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "owner",
+            "type": "pubkey"
+          },
+          {
+            "name": "mint",
+            "type": "pubkey"
+          },
+          {
+            "name": "treasureTokenAccount",
+            "type": "pubkey"
+          },
+          {
+            "name": "companyName",
+            "type": "string"
+          },
+          {
+            "name": "treasuryBump",
+            "type": "u8"
+          },
+          {
+            "name": "bump",
+            "type": "u8"
+          }
+        ]
+      }
+    }
   ]
-}
+};
