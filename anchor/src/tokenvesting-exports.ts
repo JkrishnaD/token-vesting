c54264617df400fc42c029ecf6cba507179c3950
JKrishnaD <jayak5063@gmail.com>
2025-04-24 18:08:16 +0530

    id change

--- anchor/src/tokenvesting-exports.ts
+++ anchor/src/tokenvesting-exports.ts
@@ -21,7 +21,7 @@ export function getTokenvestingProgramId(cluster: Cluster) {
     case 'devnet':
     case 'testnet':
       // This is the program ID for the Tokenvesting program on devnet and testnet.
-      return new PublicKey('coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF')
+      return new PublicKey('8QZkjV2un7BG5ys72TxncWQHM175kxDc1ptHWsg4bKwu')
     case 'mainnet-beta':
     default:
       return TOKENVESTING_PROGRAM_ID
